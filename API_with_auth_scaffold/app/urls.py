a359655d08ce153c1f30cb7d940c6c05931c0659
Johannes Anih <107084872+johannesanih@users.noreply.github.com>
2025-07-06 22:30:52 +0000

    debugged, intalled missing modules, updated requirements.txt file

--- API_with_auth_scaffold/app/urls.py
+++ API_with_auth_scaffold/app/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('todos.urls')),
+    path('api/', include('main.urls')),
     path('api/auth/', include('users.urls')),
 ]
--- API_with_auth_scaffold/main/models.py
+++ API_with_auth_scaffold/main/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from users.models import CustomUser
 
-"""
 class Example(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField(blank=True)
@@ -12,4 +11,3 @@ class Example(models.Model):
     def __str__(self):
         return self.title
 
-"""
